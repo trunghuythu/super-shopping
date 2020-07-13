9b789df482be1a37f8290f09f323eac8a868688e
Thu <thutrung@absolute.com>
2020-07-13 08:09:24 +0700

    Mark ID field for Product model

--- product-service/src/main/java/com/ttrung/supershop/product/domain/Product.java
+++ product-service/src/main/java/com/ttrung/supershop/product/domain/Product.java
@@ -6,17 +6,17 @@
  */
 package com.ttrung.supershop.product.domain;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
 @lombok.Getter
 @lombok.Setter
 public class Product {
+    @Id
     private String id;
     private String name;
     private String category;
     private String brand;
     private Double price;
-
-
 }

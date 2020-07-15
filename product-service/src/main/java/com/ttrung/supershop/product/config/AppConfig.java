4f23cc8c12e611e1961503758364a8971198cf4e
Thu <thutrung@absolute.com>
2020-07-15 11:01:54 +0700

    Enable resource server, using OAuth2 flow for authorization

---
+++ product-service/src/main/java/com/ttrung/supershop/product/config/AppConfig.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2020 Absolute Software Corporation. All rights reserved.
+ * Reproduction or transmission in whole or in part, in any form or by any means,
+ * electronic, mechanical or otherwise, is prohibited without the prior written
+ * consent of the copyright owner.
+ */
+package com.ttrung.supershop.product.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class AppConfig extends WebSecurityConfigurerAdapter {
+
+    @Primary
+    @Bean
+    public RemoteTokenServices tokenService() {
+        RemoteTokenServices tokenService = new RemoteTokenServices();
+        tokenService.setCheckTokenEndpointUrl(
+                "http://localhost:8127/oauth/token?scope=any&grant_type=client_credentials");
+        tokenService.setClientId("supershop");
+        tokenService.setClientSecret("supershop");
+        return tokenService;
+    }
+}

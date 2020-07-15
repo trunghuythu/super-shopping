3c32c229048ef49970e107a43943f7bdaa9b0497
Thu <thutrung@absolute.com>
2020-07-15 09:24:37 +0700

    Create user's cart in case none exists

--- order-service/src/main/java/com/ttrung/supershop/order/service/impl/ShoppingCartServiceImpl.java
+++ order-service/src/main/java/com/ttrung/supershop/order/service/impl/ShoppingCartServiceImpl.java
@@ -8,7 +8,6 @@ package com.ttrung.supershop.order.service.impl;
 
 import com.ttrung.supershop.order.domain.ShoppingCart;
 import com.ttrung.supershop.order.dto.ShoppingCartDto;
-import com.ttrung.supershop.order.exception.CartNotFoundException;
 import com.ttrung.supershop.order.mapper.ShoppingCartMapper;
 import com.ttrung.supershop.order.repository.ShoppingCartRepository;
 import com.ttrung.supershop.order.service.ShoppingCartService;
@@ -29,15 +28,10 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
 
     @Override
     public ShoppingCartDto updateCart(String userId, ShoppingCartDto cartForm) {
-        if (!cartRepository.existsById(userId)) {
-            throw new CartNotFoundException(String.format("The shopping cart specified by Id [%s] does not exist", userId));
-        } else {
-            cartForm.setId(userId);
-            ShoppingCart updatedCart = cartRepository.save(cartMapper.dtoToDomain(cartForm));
+        cartForm.setId(userId);
+        ShoppingCart updatedCart = cartRepository.save(cartMapper.dtoToDomain(cartForm));
 
-            return cartMapper.domainToDto(updatedCart);
-        }
-
+        return cartMapper.domainToDto(updatedCart);
     }
 
     @Override
@@ -53,8 +47,7 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
     }
 
     @Override
-    public Optional<ShoppingCartDto> getCart(String userId)
-    {
+    public Optional<ShoppingCartDto> getCart(String userId) {
         Optional<ShoppingCart> cart = cartRepository.findById(userId);
         return cart.map(cartMapper::domainToDto);
     }
